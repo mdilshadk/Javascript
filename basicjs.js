abaf8a037fbadf8482b1bd716fddbfdd2fdbb703
mdilshadk <muhammeddilshad0140@gmail.com>
2024-12-23 14:56:01 +0530

    javascript

---
+++ basicjs.js
@@ -0,0 +1,13 @@
+{
+    var x=5;
+    var y=8;
+    var sum=x+y;
+    var sub=x-y;
+    var multi=x*y;
+    var division=x/y;
+    console.log(sum);
+    console.log(sub);
+    console.log(multi);
+    console.log(division);
+}
+
